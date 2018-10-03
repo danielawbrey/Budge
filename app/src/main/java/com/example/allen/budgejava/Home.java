7fa52a70edb4fffe5f9cba82d24fc15528d8d003
danielawbrey <danielawbrey@gmail.com>
2018-10-03 14:57:25 -0700

    first commit

---
+++ app/src/main/java/com/example/allen/budgejava/Home.java
@@ -0,0 +1,28 @@
+package com.example.allen.budgejava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.PieChart;
+
+public class Home extends AppCompatActivity {
+
+    private float[]
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        PieChart chart = (PieChart) findViewById(R.id.chart);
+
+
+
+
+
+
+
+    }
+}
