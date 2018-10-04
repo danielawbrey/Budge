b7d2c180690b5b32b9e9a4d0f2bd64c5e8962019
AvoHagopian <avedis.hagopian.111@my.csun.edu>
2018-10-04 10:24:46 -0700

    Dependencies should be happy now

--- app/src/main/java/com/example/allen/budgejava/Home.java
+++ app/src/main/java/com/example/allen/budgejava/Home.java
@@ -1,10 +1,20 @@
 package com.example.allen.budgejava;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
 
@@ -19,25 +29,43 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        pie = (PieChart) findViewById(R.id.idPieChart);
+        pie = (PieChart) findViewById(R.id.chart);
+       // pie.setDescription("Sales by employee (In Thousands $) ");
+        pie.setRotationEnabled(true);
+        //pie.setUsePercentValues(true);
+        //pie.setHoleColor(Color.BLUE);
+        //pie.setCenterTextColor(Color.BLACK);
+        pie.setHoleRadius(25f);
+        pie.setTransparentCircleAlpha(0);
+        pie.setCenterText("Super Cool Chart");
+        pie.setCenterTextSize(10);
+        //pie.setDrawEntryLabels(true);
+        //pie.setEntryLabelTextSize(20);
 
-        /*
-        pieChart.setDescription("Sales by employee (In Thousands $) ");
-        pieChart.setRotationEnabled(true);
-        //pieChart.setUsePercentValues(true);
-        //pieChart.setHoleColor(Color.BLUE);
-        //pieChart.setCenterTextColor(Color.BLACK);
-        pieChart.setHoleRadius(25f);
-        pieChart.setTransparentCircleAlpha(0);
-        pieChart.setCenterText("Super Cool Chart");
-        pieChart.setCenterTextSize(10);
-        //pieChart.setDrawEntryLabels(true);
-        //pieChart.setEntryLabelTextSize(20);
-        */
 
         addDataSet();
 
+        pie.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                int pos1 = e.toString().indexOf("(sum): ");
+                String sales = e.toString().substring(pos1 + 7);
 
+                for(int i = 0; i < spent.length; i++){
+                    if(spent[i] == Float.parseFloat(sales)){
+                        pos1 = i;
+                        break;
+                    }
+                }
+                String employee = categories [pos1 + 1];
+                Toast.makeText(Home.this, "Employee " + employee + "\n" + "Sales: $" + sales + "K", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
 
 
 
@@ -50,7 +78,7 @@ public class Home extends AppCompatActivity {
         ArrayList<String> xVar = new ArrayList<>();
 
         for(int i = 0; i < spent.length; i++){
-            yVar.add(new PieEntry(yVar[i] , i));
+            yVar.add(new PieEntry(spent[i], i));
         }
 
         for(int i = 1; i < categories.length; i++){
@@ -62,7 +90,7 @@ public class Home extends AppCompatActivity {
         pieDataSet.setSliceSpace(2);
         pieDataSet.setValueTextSize(12);
 
-        //add colors to dataset
+        //add colors to data set
         ArrayList<Integer> colors = new ArrayList<>();
         colors.add(Color.GRAY);
         colors.add(Color.BLUE);
@@ -75,13 +103,13 @@ public class Home extends AppCompatActivity {
         pieDataSet.setColors(colors);
 
         //add legend to chart
-        Legend legend = pieChart.getLegend();
+        Legend legend = pie.getLegend();
         legend.setForm(Legend.LegendForm.CIRCLE);
         legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
 
         //create pie data object
         PieData pieData = new PieData(pieDataSet);
-        pieChart.setData(pieData);
-        pieChart.invalidate();
+        pie.setData(pieData);
+        pie.invalidate();
     }
 }
